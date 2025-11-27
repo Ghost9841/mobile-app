ae44a116e2980946abc47b64ef643c98ac928478
Ghost9841 <asubedi778@gmail.com>
2025-11-27 11:19:20 +0545

    Native wind successfully done and installed

--- packages/client/app/index.tsx
+++ packages/client/app/index.tsx
@@ -1,12 +1,19 @@
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
+import { Link } from "expo-router";
 import "./global.css";
- 
+
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-500">
         Welcome to Nativewind!
       </Text>
+
+      <Link href="/(auth)/login/page" asChild>
+        <TouchableOpacity className="mt-4 rounded bg-blue-500 p-4">
+          <Text className="text-white text-2xl">Go to Login</Text>
+        </TouchableOpacity>
+      </Link>
     </View>
   );
 }
